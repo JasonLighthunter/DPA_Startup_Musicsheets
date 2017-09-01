9f5a03c2d8447f24831398324c605eb4091a3bc2
Stev43 <steven.v.d.vorm@hotmail.com>
2017-09-01 13:56:58 +0200

    Added tests + NoteFactory

--- DPA_Musicsheets/Models/SJRest.cs
+++ DPA_Musicsheets/Models/SJRest.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DPA_Musicsheets.Models
 {
-    class SJRest : SJBaseNote
+    public class SJRest : SJBaseNote
     {
     }
 }
---
+++ DPA_Musicsheets_Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DPA_Musicsheets_Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DPA_Musicsheets_Test")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4acdb519-a080-4263-ad40-28fd465e0ad4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
