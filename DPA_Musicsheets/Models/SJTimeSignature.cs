de7e3126d1385a856feef4076dc398b1e388a2be
Stev43 <steven.v.d.vorm@hotmail.com>
2017-09-04 14:08:43 +0200

    added bar and timesignature POCO's added Lilypondparser (files)

---
+++ DPA_Musicsheets/Models/SJTimeSignature.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPA_Musicsheets.Models
+{
+    class SJTimeSignature
+    {
+        private uint _numberOfBeatsPerBar;
+
+        public uint NumberOfBeatsPerBar
+        {
+            get { return _numberOfBeatsPerBar; }
+            set { _numberOfBeatsPerBar = value; }
+        }
+
+        private uint _noteValueOfBeat;
+
+        public uint NoteValueOfBeat
+        {
+            get { return _noteValueOfBeat; }
+            set { _noteValueOfBeat = value; }
+        }
+    }
+}
---
+++ DPA_Musicsheets_Test/LilypondParserTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DPA_Musicsheets.Managers;
+using DPA_Musicsheets.Models;
+
+namespace DPA_Musicsheets_Test
+{
+    [TestClass]
+    public class LilypondParserTest
+    {
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            SJBaseNote note = SJLilypondParser.ParseNote();
+            Assert.IsNotNull(note);
+        }
+    }
+}
