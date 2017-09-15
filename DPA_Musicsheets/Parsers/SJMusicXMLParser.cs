93059cabfacaa4a1c9cb55c590ecb19e7e5ec362
Stev43 <steven.v.d.vorm@hotmail.com>
2017-09-15 09:37:28 +0200

    added XMLParser file

---
+++ DPA_Musicsheets/Parsers/SJMusicXMLParser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DPA_Musicsheets.Models;
+
+namespace DPA_Musicsheets.Parsers
+{
+    class SJMusicXMLParser : ISJParser<String>
+    {
+        public string ParseFromSJSong(SJSong song)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SJSong ParseToSJSong(string data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
